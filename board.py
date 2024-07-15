c408dc7dacd5436ef9852879197271dc808196bb
Ashley Boswell <boswelas@oregonstate.edu>
2024-07-14 18:03:07 -0700

    first commit

---
+++ board.py
@@ -0,0 +1,18 @@
+class Board:
+   
+    def __init__(self, size):
+        self.size = size
+        self.board = self.generate_board()
+        
+    def generate_board(self):
+        board = []
+        for row in range(self.size):
+            a = []
+            for column in range(self.size):
+                a.append("-")
+            board.append(a)
+        return board    
+    
+    def print_board(self):
+        for row in self.board:
+            print(" ".join(row))
---
+++ ship.py
@@ -0,0 +1,12 @@
+class Ship:
+    
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+        self.positions = []
+
+    def return_name(self):
+        return self.name
+        
+    def return_size(self):
+        return self.size
